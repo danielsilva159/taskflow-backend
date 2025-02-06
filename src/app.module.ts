73167c9100ec6a2746cdec9027b2f6248aaac6c1
danielsilva159 <silva_120000@yahoo.com.br>
2025-02-06 15:31:00 -0300

    create project and starting database

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'user',
+      password: process.env.DB_PASS || 'password',
+      database: process.env.DB_NAME || 'taskflow',
+      autoLoadEntities: true,
+      synchronize: true, // Apenas para desenvolvimento
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
